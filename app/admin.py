e988074c7199ce4913bc3f3477206bb585320774
Alexey Gavrikov <alex.gavr97@gmail.com>
2017-12-02 21:09:32 +0300

    Добавил данных в базу данных Зарегистрировал в админке объекты Исправил помарки в models.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models as app_models
+admin.site.register(app_models.Student)
+admin.site.register(app_models.Teacher)
+
+admin.site.register(app_models.Course)
+admin.site.register(app_models.Merit)
+
+admin.site.register(app_models.MeetingType)
+admin.site.register(app_models.Meeting)
+
+admin.site.register(app_models.TeacherTeaches)
+admin.site.register(app_models.StudentEnrolled)
+
+admin.site.register(app_models.TeacherAttends)
+admin.site.register(app_models.StudentAttends)
+
+admin.site.register(app_models.GradeAction)
+
+
