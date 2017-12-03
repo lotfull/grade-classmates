f27f2af1b1b02072f7e332d1fa37f940df22a18e
Kam Lotfull <kamlotfull@gmail.com>
2017-12-03 11:53:55 +0300

    generate_courses method added

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+import random
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -98,6 +100,8 @@ class DataManager(models.Manager):
 
     users_names = ["Fadeev", "Terentyev", "Kozlov", "Kuzmin", "Fedorov", "Shcherbakov", "Dmitriev", "Yudin", "Belousova", "Gerasimov", "Seleznyov", "Prokhorov", "Bobrov", "Birds", "Crane", "Zuyev", "Karpov", "Vladimir", "Kolobov", "Nesterov", "Kondratyev", "Petrov", "Sparrows", "Maksimov", "Isayev", "Frolov", "Shchukin", "Alexander", "Tretyakov", "Vorontsov", "Kosheleva", "Efimova", "Lobanov", "Sorokin", "Belousova", "Boer", "Seleznyov", "Sukhanov", "Morozov", "Doronin", "Vorob", "Makarov", "Nesterov", "Sokolov", "Beetles", "Nekrasov", "Gavrilov", "Wings", "Simonov", "Myasnikova", "Cossacks", "Sparrows", "Kovalev", "Ermakova", "Larionov", "Vorob", "Sokolov", "Mironov", "Davidoff", "Loginova", "Zhdanov", "Bobylev", "Subbotina", "Dorofeev", "Popova", "Voronova", "Index", "Guryev", "Karpov", "Zhdanov", "Orlov", "Kulikova", "Sharova", "Ignatova", "Myasnikov", "Boar", "Panov", "Mammoth", "O", "Bykov", "Panova", "Belousova", "Gushchin", "Koshelev", "Krasilnikov", "Fomin", "Danilov", "Savel", "Pancakes", "Vlasov", "Mikhaylov", "Smith", "Pavlov", "Ponomareva", "Nesterov", "Mikheev", "Frolov", "Konovalov", "Kirillov", "Kolesnikov"]
 
+    courses_names = ["Algebra", "Algorithms and data structures", "Algorithms and data structures", "Analysis of markets and competitiveness", "English", "computer Architecture and operating systems", "Safety", "Introduction to object-oriented programming", "Introductory research seminar", "Discrete mathematics", "Discrete mathematics ", "Discrete optimization", "Differential equations, History, Linear algebra and geometry", "macroeconomics", "Mathematical analysis", "Machine learning 1", "Machine learning on big data", "Interdisciplinary course work", "Microeconomics", "research seminar in Distributed systems", "Scientific seminar", "Independent English exam ", "Continuous optimization, Parallel and distributed computing", "Software project", "Project Software project 1", "Psychology in it", "Theory of databases", "Theory of probability and mathematical statistics", "Teaching", "Teaching practice", "Physical culture"]
+
     @staticmethod
     def generate_data():
         for i, name in enumerate(DataManager.users_names):
@@ -139,3 +143,24 @@ class DataManager(models.Manager):
             print(user, "is NOBODY")
             nobody_n += 1
         print("stud_n = {}, teach_num = {}, nobody_n = {}".format(stud_n, teach_num, nobody_n))
+
+    @staticmethod
+    def generate_courses():
+        courses_num = 5
+        students_per_course = 20
+        start_date = datetime(2017, 9, 1)
+        end_date = datetime(2018, 6, 30)
+        few_courses_names = random.sample(DataManager.courses_names, 5)
+        for i, course_name in enumerate(few_courses_names):
+            course, created = Course.objects.get_or_create(
+                name=course_name,
+                description="The course of {} give student basic skills in {}".format(course_name, course_name),
+                start_date=start_date,
+                end_date=end_date,
+            )
+            if created:
+                print("created", course)
+            else:
+                print("existing", course)
+            course.save()
+
