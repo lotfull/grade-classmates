d3c90610babf58186e218b47aa64f617707d6935
nialeksandrov <Kredshow@gmail.com>
2017-12-03 16:55:20 +0300

    Final version of dashboard is done

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
+from collections import Counter
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 
-from .models import StudentEnrolled
+from .models import GradeAction, StudentAttends, StudentEnrolled
 
 def main_page(request):
     return HttpResponse("Main Page")
@@ -26,6 +27,34 @@ def dashboard(request):
                            'end_date': student_enrolled.course.end_date }
             context.get('courses_enrolled').append(course_info)
 
-    print(current_user, 'enters in dashboard')
+    students_attended = StudentAttends.objects.all()
+    for student_attended in students_attended:
+        if student_attended.student.user == current_user:
+            meeting = student_attended.meeting
+            meeting_info = {}
+            meeting_info['date'] = meeting.date
+            meeting_info['place'] = meeting.place
+            meeting_info['course'] = meeting.course
+            meeting_info['meeting_type'] = meeting.meeting_type
+            context.get('meeting_enrolled').append(meeting_info)
+
+    grade_actions = GradeAction.objects.all().filter(graded = current_user)
+    context['positive_qualities'] = {}
+    context['negative_qualities'] = {}
+    for grade in grade_actions:
+        merit = grade.merit
+        if merit.description == '+':
+            if context['positive_qualities'].get(merit.name) == None:
+                context['positive_qualities'][merit.name] = 1
+            else:
+                context['positive_qualities'][merit.name] += 1
+        else:
+            if context['negative_qualities'].get(merit.name) == None:
+                context['negative_qualities'][merit.name] = 1
+            else:
+                context['negative_qualities'][merit.name] += 1
+
+    for i in context['positive_qualities'].items():
+        print(i[0], i[1])
 
     return render(request, 'app/dashboard.html', context)
